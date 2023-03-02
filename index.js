fcec9431e093c4018674f780c38d723c136ad5bf
njambi-cloud <christinemacharia64@gmail.com>
2023-03-02 05:29:51 +0300

    Adds the merge sort algorithm

---
+++ index.js
@@ -0,0 +1,28 @@
+function mergeSort(arr) {
+    //Base case for our recursion
+    if(arr.length<2){
+        return arr
+    }
+
+    const mid = Math.floor((arr.length)/2)
+    const leftArr=arr.slice(0,mid)
+    const rightArr=arr.slice(mid)
+
+    return merge(mergeSort(leftArr),mergeSort(rightArr))
+}
+
+function merge(leftArr,rightArr) {
+    const sortedArr=[]
+
+    while(leftArr.length>0 && rightArr.length>0){
+        if(leftArr[0]<= rightArr[0]){
+            sortedArr.push(leftArr.shift())
+        }
+        else{
+            sortedArr.push(rightArr.shift())
+        }
+    }
+    return sortedArr.concat(leftArr,rightArr)
+}
+
+console.log(mergeSort([90,55,700,4,67,89]))
